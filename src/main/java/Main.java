ac558f0181cc3790fe35c471fabdfb018aaec494
Nathan <nathan@Zipcoders-MacBook-Pro-10.local>
2023-10-26 20:58:53 -0400

    Final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,38 @@
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+        //This is a "random" class
+        Random rand = new Random();
+
+        int randomNumber = rand.nextInt(25) + 1;
+        System.out.println("Random number is " + randomNumber);
+        int tryCount = 0;
+        while (true) {
+            System.out.println("Pick a number between 1-25 ");
+
+            Scanner scanner = new Scanner(System.in);
+            int playerGuess = scanner.nextInt();
+            tryCount++;
+            if (playerGuess == randomNumber) {
+                System.out.println("correct guess");
+                System.out.println("It took you " + tryCount + " tries" );
+                break;
+            } else if (randomNumber > playerGuess) {
+                System.out.println("too small");
+            } else {
+                System.out.println("too large");
+            }
+        }
     }
+
 }
